8e79d8f1b7e8b8a4dae7f0b5bf9c62852e4d4121
beautyzhangyy <1710733562@qq.com>
2023-05-08 09:59:23 +0800

    2

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+// Store all user information
 const chattingUsers = new Map();
-
-// Serve static files
 app.use(express.static(__dirname + "/public"));
 
 http.listen(3000, () => {
@@ -14,43 +13,62 @@ http.listen(3000, () => {
 io.on("connection", (socket) => {
   console.log("----- A user joins the chat room -----");
 
-  // Add a new user to the online user list when the user is connected
-  chattingUsers.set(socket.id, { id: socket.id });
-
-  // Send a welcome message to the new user
+  // Message received by a new user
   socket.emit("chat status", "----- You join the chat room -----");
 
-  // Broadcast a message to all other connected clients
+  // Message received by other users after a new user is added
   socket.broadcast.emit(
     "chat status",
-    "----- A user has left the chat room -----"
+    "----- A user joins the chat room -----"
   );
 
-  // Listen for chat contents
-  socket.on("chat content", (msg) => {
-    console.log("message: " + msg);
-    io.emit("chat content", msg); // Broadcast the message to all connected clients
+  // Listening user is sending status message after input
+  socket.on("chat status", (typeMessage) => {
+    io.emit("chat status", typeMessage);
   });
 
-  // Handle user leave
+  // A message is generated after the listener sends a message
+  socket.on("chat content", (content) => {
+    console.log("message: " + content);
+    io.emit("chat content", content); // Broadcast the message to all connected clients
+  });
+
+  // Listen for the user to click away to leave the chat
   socket.on("user leave", (reason) => {
     console.log("user leave", reason);
-    // Remove a user from the online user list when the user is disconnected
+    // Delete user information
     chattingUsers.delete(socket.id);
-    // Broadcast a message to all other connected clients
+    // Send status information to other users
     socket.broadcast.emit(
       "chat status",
       "----- A user has left the chat room -----"
     );
   });
 
-  // Processing requests to get a list of online users
-  socket.on("get user list", () => {
-    const userList = Array.from(chattingUsers.values());
-    socket.emit("user list", userList);
+  // Listen for the user to close the page and leave the chat
+  socket.on("disconnect", (reason) => {
+    console.log("user leave", reason);
+    // Delete user information
+    chattingUsers.delete(socket.id);
+    // Send status information to other users
+    socket.broadcast.emit(
+      "chat status",
+      "----- A user has left the chat room -----"
+    );
   });
 
-  socket.on("send user list", (message) => {
-    socket.emit("chat content", message);
+  // The user list is generated after the chat interface is enabled
+  const userList = Array.from(chattingUsers.values());
+  socket.emit("create user list", userList);
+
+  // Generate user list
+  socket.on("user list", () => {
+    const userList = Array.from(chattingUsers.values());
+    io.emit("create user list", userList);
+  });
+
+  // Add information about a new user
+  socket.on("add user list", (newName) => {
+    chattingUsers.set(socket.id, { name: newName });
   });
 });
